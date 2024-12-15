bb08df37cc05cd0dd0a1a45dfa3f08b856be11e4
Leonardo Vásquez Nureña <121217531+DevNRoot@users.noreply.github.com>
2024-12-15 15:42:09 -0500

    Initial Commit

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import MoviePage from './Component/MoviePage'
+
+function App() {
+
+  return (
+    <>
+      <div className=''>
+        <MoviePage />
+      </div>
+    </>
+  )
+}
+
+export default App
